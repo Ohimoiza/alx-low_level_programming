9878c7c434305ac83061207d74d8b771e763e6dc
Ohimoiza <ohimoiza12@gmail.com>
2022-12-09 04:11:59 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,34 @@
-#include <stdio.>
+#include <stdio.h>
+
+
 
 /**
- * main - Prints the alphabet.
  *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+ *  * main - prints the alphabet in lowercase, and then in uppercase,
+ *
+ *   * followed by a new line
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-	for (i = 0, i < 26, i++)
-	{
-		putchar(alp[i]);
-	}
+int main(void)
+	
+{
+
+	int ch;
+
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar(ch);
+
 	putchar('\n');
+
 	return (0);
 }
