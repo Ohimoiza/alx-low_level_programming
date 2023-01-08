4ba67cbb7bb79612cf2c644892917faa3789e20a
Ohimoiza <ohimoiza12@gmail.com>
2023-01-08 05:32:01 -0800

    libraries in C

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Create an empty archive file
-ar rcs liball.a
+gcc -Wall -Werror -Wextra -pendantic -c *.c
+ar rcs libmy.a
 
 # Iterate over all .c files in the current directory
 for file in *.c; do
   # Compile the .c file into an object file
   gcc -c "$file"
   # Add the object file to the library
-  ar rcs liball.a "${file%.c}.o"
+  ar rcs libmy.a "${file%.c}.o"
 done
