1a654c2814b6661be59d542f0ad80e1a39d385bb
Ohimoiza <ohimoiza12@gmail.com>
2023-01-09 04:59:38 -0800

    A function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * str_concat - concatenates two strings
@@ -47,3 +47,4 @@ char *str_concat(char *s1, char *s2)
 
 	return (s3);
 }
+
